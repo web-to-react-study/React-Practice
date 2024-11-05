9b1f8e0a08fddbf7993ba6b40a5258b0c9d7672f
SUJUNG CHOI <suho3938@gmail.com>
2024-11-05 16:36:36 +0900

    week03_HW

---
+++ src/webtoonData.js
@@ -0,0 +1,92 @@
+const webtoonData = [
+    /* 추천 웹툰 */ 
+    { // 청춘러브썸
+        isRecommend: true, 
+        image: "https://image-comic.pstatic.net/webtoon/828920/thumbnail/titledescimage/frontImage_391fbfdc-070f-4373-be3a-3560ac5f55af.png",
+        title: "청춘 러브썸",
+        author: "홍덕 / NEMONE",
+        episode: "8화",
+        rating: "★9.98",
+        update: 0,
+        views: 0
+    },
+    { // 나만 보는 탑 공략집
+        isRecommend: true, 
+        image: "https://image-comic.pstatic.net/webtoon/823434/thumbnail/titledescimage/frontImage_47f5222e-2bd7-490e-b8e0-183e7b7fd0e1.png",
+        title: "나만 보는 탑 공략집",
+        author: "사도 / X-NAME / 쾌조",
+        episode: "26화",
+        rating: "★9.94",
+        update: 0,
+        views: 0
+    },
+    { // 그냥 선생님 
+        isRecommend: true, 
+        image: "https://image-comic.pstatic.net/webtoon/804333/thumbnail/titledescimage/frontImage_ecea4635-1755-41f0-8160-da8f615f93db.png",
+        title: "그냥 선생님",
+        author: "연일",
+        episode: "182화",
+        rating: "★9.98",
+        update: 0,
+        views: 0
+    },
+
+    /* 전체 화요 웹툰 */
+    { // 김부장 
+        isRecommend: false, 
+        image: "https://image-comic.pstatic.net/webtoon/783053/thumbnail/thumbnail_IMAG21_d7732f14-26da-4e35-8762-660cc87b53f1.jpg",
+        title: "김부장",
+        author: "박민사, 남자의 이야기",
+        episode: 0,
+        rating: "★9.59",
+        ratingNumber: 9.59,
+        update: 0, 
+        views: 6
+    },
+    { // 마루는 강쥐
+        isRecommend: false, 
+        image: "https://image-comic.pstatic.net/webtoon/796152/thumbnail/thumbnail_IMAG21_26b9c1d8-ca2d-4fc7-87ea-a3334634236a.jpg",
+        title: "마루는 강쥐",
+        author: "모죠",
+        episode: 0,
+        rating: "★9.98",
+        ratingNumber: 9.98,
+        update: 3,
+        views: 7
+    },
+    {
+        isRecommend: false, 
+        image: "https://image-comic.pstatic.net/webtoon/821192/thumbnail/thumbnail_IMAG21_156483ac-3740-4cbf-8150-0f24595bae52.jpg",
+        title: "좋아? 죽어!",
+        author: "김용키, 박만사 / 죠",
+        episode: 0,
+        rating: "★9.48",
+        ratingNumber: 9.48,
+        update: 2,
+        views: 9
+    },
+    {
+        isRecommend: false, 
+        image: "https://image-comic.pstatic.net/webtoon/784248/thumbnail/thumbnail_IMAG21_20a8aca7-23d3-4bc0-9916-a31ebc884ca8.jpg",
+        title: "내가 키운 S급들",
+        author: "seri / 비완 / 근서",
+        episode: 0,
+        rating: "★9.48",
+        ratingNumber: 9.48, 
+        update: 1,
+        views: 10
+    },
+    {
+        isRecommend: false, 
+        image: "https://image-comic.pstatic.net/webtoon/833255/thumbnail/thumbnail_IMAG21_504f6152-8a60-42a0-a1c0-43cd77f9c430.jpg",
+        title: "낢이 사는 이야기",
+        author: "서나래",
+        episode: 0,
+        rating: "★9.95",
+        ratingNumber: 9.95, 
+        update: 4,
+        views: 8
+    }
+];
+
+export default webtoonData;
