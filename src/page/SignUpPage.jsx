da9c5155a4770c98980589c9ac180f46389715f1
dhleesep9 <50320768+dhleesep9@users.noreply.github.com>
2024-11-12 16:10:01 +0900

    4주차 1차 제출

--- src/page/SignUpPage.jsx
+++ src/page/SignUpPage.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 function SignUpPage() {
   /**
@@ -9,13 +10,18 @@ function SignUpPage() {
    * - ID는 유저가 소문자로 입력해도, UI에는 대문자로 나오도록 구현
    * - onSubmit 이벤트 발생 시, console에 현재 사용자 input(ID, PASSWORD 모두) 을 출력
    */
-
-  return (
-    <Container>
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(ID, '\n', PASSWORD);
+  };
+  const [id, ID] = useState('');
+  const [password, PASSWORD] = useState('');
+  return (  
+    <Container onSubmit={onSubmit}>
       <h1>회원가입</h1>
-      <Input name="id" placeholder="ID 입력 / 대문자만 가능" />
-      <Input name="password" placeholder="password 입력 / 8자 이상" />
-      <Button disabled>가입</Button>
+      <Input name="id" placeholder="ID 입력 / 대문자만 가능" value={id} onChange={(e) => ID(e.target.value.toUpperCase())}/>
+      <Input name="password" placeholder="password 입력 / 8자 이상" value={password} onChange={(e) => PASSWORD(e.target.value)}/>
+      <Button disabled={password.length < 8 || !id.length}>가입</Button>
     </Container>
   );
 }
@@ -25,19 +31,16 @@ export default SignUpPage;
 const Container = styled.form`
   margin: 0 auto;
   padding: 0 20px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 8px;
-
   max-width: 360px;
 `;
 
 const Input = styled.input`
   width: 100%;
   height: 44px;
-
   border: 1px solid gray;
   border-radius: 8px;
   padding: 8px;
@@ -47,19 +50,14 @@ const Button = styled.button`
   width: 100%;
   height: 44px;
   margin-top: 16px;
-
   background-color: #03c75a;
   border: none;
   border-radius: 8px;
-
   color: white;
-
   cursor: pointer;
-
   &:hover {
     background-color: #00ac4e;
   }
-
   &:disabled {
     background-color: #b1b1b1;
     cursor: not-allowed;
