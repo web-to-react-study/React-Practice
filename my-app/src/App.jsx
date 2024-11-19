4d551fba0fd7a60b004b73d20c401d2cf61bbad7
unknown <rlaxorudcpffh@gmail.com>
2024-11-19 13:01:35 +0900

    week5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,14 @@
+import {Route, Routes } from 'react-router-dom';
 import SignUpPage from './page/SignUpPage';
+import MainPage from './page/MainPage';
 
 function App() {
-  return <SignUpPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+  );
 }
 
 export default App;
