ae3f922fc43593cf6175d460b32336b8a61c087d
choisio2 <suho3938@gmail.com>
2024-11-19 18:08:44 +0900

    week05_HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import SignUpPage from './page/SignUpPage';
+import WebtoonPage from './page/WebtoonPage';
 
 function App() {
-  return <SignUpPage />;
+  return (
+    <div>
+      <nav>
+        <Link to="/signup">
+          <button>SignUp Page</button>
+        </Link>
+        <Link to="/webtoon">
+          <button>Webtoon Page</button>
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" />
+        <Route path="/webtoon" element={<WebtoonPage/>} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
